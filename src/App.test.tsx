a0512d89a46837e4a16630cbf7afd8ab4ec61f1f
chiomanwocha <chiomanwocha17@gmail.com>
2026-09-16 18:13:27 +0100

    test: set up vitest and testing library

---
+++ src/App.test.tsx
@@ -0,0 +1,11 @@
+import { render, screen } from '@testing-library/react'
+
+import App from './App'
+
+describe('App', () => {
+  it('renders the dashboard heading', () => {
+    render(<App />)
+
+    expect(screen.getByRole('heading', { name: /novabiz merchant dashboard/i })).toBeInTheDocument()
+  })
+})
